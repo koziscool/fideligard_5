ffe6b85cc87577f5721899cd69bfaf44f163ff94
koziscool <kozjob@yahoo.com>
2016-07-11 16:05:33 -0500

    default route

--- app/app.js
+++ app/app.js
@@ -2,3 +2,8 @@
 var fido = angular.module('fideligard',['ui.router']);
 console.log(fido);
 
+fido.config( function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise('/stocks');
+
+})
